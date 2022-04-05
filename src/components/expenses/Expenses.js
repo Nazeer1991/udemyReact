e659285dc811294e4b6f2e2a1fcab6764fc0b057
Nazeer <nazeersathak@gmail.com>
2022-04-05 12:51:28 +0530

    three different ways of conditional rendering

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -12,20 +12,51 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filter;
   });
+
+  let expensesContent = <p>No Expenses Found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          key={expense.id}
+        />
+      );
+    });
+  }
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filter} onFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => {
-          return (
-            <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-              key={expense.id}
-            />
-          );
-        })}
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No Expenses Found</p>
+        ) : (
+          filteredExpenses.map((expense) => {
+            return (
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                key={expense.id}
+              />
+            );
+          })
+        )} */}
+        {/* {filteredExpenses.length === 0 && <p>No Expenses Found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => {
+            return (
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                key={expense.id}
+              />
+            );
+          })} */}
       </Card>
     </div>
   );
